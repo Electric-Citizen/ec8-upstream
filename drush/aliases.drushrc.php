a1b3384ff395af72138cb943d643226061522827
Tim Broeker <tim@electriccitizen.com>
2018-01-17 12:51:21 -0600

    update vm config

--- drush/aliases.drushrc.php
+++ drush/aliases.drushrc.php
@@ -1,6 +1,6 @@
 <?php
-$aliases['newupstream.dev'] = array(
-  'uri' => 'dev-newupstream.pantheonsite.io',
+$aliases['ec8-upstream.dev'] = array(
+  'uri' => 'dev-ec8-upstream.pantheonsite.io',
   'db-url' => 'mysql://pantheon:a6872daf0abe4ecbabac7d185e6d8e82@dbserver.dev.8be50dbc-4463-43b5-bd18-d83046623820.drush.in:12588/pantheon',
   'db-allows-remote' => TRUE,
   'remote-host' => 'appserver.dev.8be50dbc-4463-43b5-bd18-d83046623820.drush.in',
@@ -12,10 +12,10 @@ $aliases['newupstream.dev'] = array(
   ),
 );
 
-$aliases['newupstream.local'] = array(
-  'uri' => 'newupstream.local',
+$aliases['ec8-upstream.local'] = array(
+  'uri' => 'ec8-upstream.local',
   'root' => '/var/www/drupalvm/web',
-  'remote-host' => 'newupstream.local',
+  'remote-host' => 'ec8-upstream.local',
   'remote-user' => 'vagrant',
   'ssh-options' => '-o "SendEnv PHP_IDE_CONFIG PHP_OPTIONS XDEBUG_CONFIG" -o PasswordAuthentication=no -i "' . (getenv('VAGRANT_HOME') ?: drush_server_home() . '/.vagrant.d') . '/insecure_private_key"',
   'path-aliases' => array(
